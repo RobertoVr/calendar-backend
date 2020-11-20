93acc6541434f0fecb6368e0173564220f0f5235
Roberto Valdez <robert.tito94@gmail.com>
2020-11-20 15:59:10 -0600

    token jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,100 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
+const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
 
 // api/auth/
-const loginUsuario = (req, resp = response) => {
+const loginUsuario = async (req, resp = response) => {
     const { email, password } = req.body;
 
-    resp.status(200).send({
-        ok: true,
-        msg: 'Login',
-        email,
-        password
-    });
+    try {
+        let usuario = await Usuario.findOne({ email });
+        if (!usuario) {
+            return resp.status(400).send(
+                {
+                    ok: false,
+                    msg: 'El usuario no existe con ese email'
+                }
+            );
+        }
+
+        // Confirmar los passwords
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+
+        if (!validPassword) {
+            return resp.status(400).send(
+                {
+                    ok: false,
+                    msg: 'Password incorrecto'
+                }
+            );
+        }
+
+        // Generar el JSON web token
+        const token = await generarJWT(usuario.id, usuario.name);
+
+        resp.status(200).send(
+            {
+                ok: true,
+                uid: usuario.id,
+                name: usuario.name,
+                token
+            }
+        )
+
+    } catch (error) {
+        resp.status(500).send(
+            {
+                ok: false,
+                msg: 'Por favor hable con el administrador'
+            }
+        );
+    }
 }
+
 // api/auth/new
-const crearUsuario = (req, resp = response) => {
+const crearUsuario = async (req, resp = response) => {
 
-    const { name, email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    resp.status(201).send({
-        ok: true,
-        msg: 'Crear usuario',
-        name,
-        email,
-        password
-    });
+        let usuario = await Usuario.findOne({ email });
+
+        if (!!usuario) {
+            return resp.status(400).send(
+                {
+                    ok: false,
+                    msg: 'El usuario ya existe'
+                }
+            )
+        }
+
+        usuario = new Usuario(req.body);
+
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        await usuario.save();
+        // generar el token
+        const token = await generarJWT(usuario.id, usuario.name);
+
+        resp.status(201).send(
+            {
+                ok: true,
+                uid: usuario.id,
+                name: usuario.name,
+                token
+            }
+        );
+    } catch (error) {
+        resp.status(500).send(
+            {
+                ok: false,
+                msg: 'Por favor hable con el administrador'
+            }
+        );
+    }
 }
 
 // api/auth/renew
