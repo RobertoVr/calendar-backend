23866858d5cf3d9183d95e75093915d16c759672
Roberto Valdez <robert.tito94@gmail.com>
2020-11-21 11:18:40 -0600

    config vars

--- config/config.js
+++ config/config.js
@@ -2,3 +2,4 @@ require('dotenv').config();
 
 process.env.PROT = process.env.PORT || 8080;
 process.env.MONGODB_CONNECTION = process.env.MONGODB_CONNECTION || 'mongodb://localhost:27017/test';
+process.env.SECRET_JWT_SEED = process.env.SECRET_JWT_SEED || 'seedTest';
